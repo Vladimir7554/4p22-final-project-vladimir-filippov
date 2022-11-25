e736c80962e9d64db9407d21a451f69b18e2eece
Vladimir <vladimir@zabweb.ru>
2022-11-25 22:44:45 +0700

    backup of final attestation

--- src/pages/BasketPage.jsx
+++ src/pages/BasketPage.jsx
@@ -1,20 +1,82 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import AppContext from "../context";
+import {API_URL} from "../views/Catalog/Catalog";
+import product from "../views/Product/Product";
+
 
 const BasketPage = () => {
-    const { basketItems, setBasketItems } = useContext(AppContext)
+    const {basketItems, setBasketItems} = useContext(AppContext)
+    const totalAmount = basketItems.reduce((total, { amount }) => total += amount, 0)
+    const [products, setProducts] = useState([])
 
-    let totalAmount = 0
+ const fetchProducts = () => {
+        fetch(API_URL)
+            .then((response) => response.json())
+            .then ((responseData) => {
+                const allProducts = responseData.flowerlist
+                const newProducts = []
 
-    basketItems.forEach(({ amount }) => totalAmount += amount)
+                allProducts.forEach((product) => {
+                    const productInBasket = basketItems.find(( { id }) => id === product.productId)
+
+                    if (productInBasket) {
+                        const { amount } = productInBasket
+                        newProducts.push({
+                            ...product,
+                            amount
+                        })
+                    }
+                })
+                setProducts(newProducts)
+            })
+    }
+
+    const onRemoveProductButtonClick = (id) => {
+        const newBasketItems = basketItems.filter((basketItem) => basketItem.id !== id)
+        const newProducts = products.filter(({ productId }) => productId !== id)
+
+        setBasketItems(newBasketItems)
+        setProducts(newProducts)
+    }
+
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
 
     return (
         <div>
             BASKET PAGE
-            {/*<div>Total Price: {basketTotalPrice.toFixed(2)} $</div>*/}
             <div>Total Amount: {totalAmount}</div>
+            <ul>
+            {products.map((product) => {
+              const {
+                  category,
+                  name,
+                  price,
+                  productId,
+                  amount,
+            } = product
+               return (
+                <li>
+                    <button onClick={() => onRemoveProductButtonClick(productId)}>x</button>
+                    <img
+                    src="https://placekitten.com/146/146"
+                    alt={name}
+                    width="146"
+                    height="146"
+                    loading="lazy"
+                />
+                    <div>{name}</div>
+                <div>{category}</div>
+                <div>Price: {price} $ x {amount}</div>
+                <div>Total: {price * amount} $</div>
+                </li>
+               )
+            })}
+            </ul>
         </div>
-    );
-};
+    )
+}
 
 export default BasketPage;
